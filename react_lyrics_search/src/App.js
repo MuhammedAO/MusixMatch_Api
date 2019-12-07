00964c16c61dcf3ce4af2b0e9d626f0561db7154
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-12-07 01:43:00 +0100

    Initial context

--- react_lyrics_search/src/App.js
+++ react_lyrics_search/src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
+   
+    <Router>
+     <Fragment>
      <Navbar/>
-    </div>
+     <div className="container">
+       <Switch>
+         <Route exact path="" component={Index}/>
+       </Switch>
+     </div>
+     </Fragment>
+    </Router>
+    
   );
 }
 
---
+++ react_lyrics_search/src/components/layout/Index.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+import Tracks from '../tracks/Tracks'
+
+
+function Index() {
+    return (
+      <Fragment>
+       <Tracks/>
+      </Fragment>
+    )
+}
+
+export default Index
