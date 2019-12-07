b70997b5f6c7aa0cd276fe4f16c0d114c6187f06
Muhammed ogunsanya <mspeaks910@gmail.com>
2019-12-07 17:05:47 +0100

    Lyrics page and CORS error handling

--- react_lyrics_search/src/App.js
+++ react_lyrics_search/src/App.js
@@ -1,6 +1,7 @@
 import React,{Fragment} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
+import Lyrics from './components/tracks/Lyrics';
 import Index from './components/layout/Index';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import { Provider } from './context';
@@ -15,7 +16,8 @@ function App() {
      <Navbar/>
      <div className="container">
        <Switch>
-         <Route exact path="" component={Index}/>
+         <Route exact path="/" component={Index}/>
+         <Route exact path="/lyrics/track/:id" component={Lyrics}/>
        </Switch>
      </div>
      </Fragment>
